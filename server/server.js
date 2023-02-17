60417f473ee1d980a50d048527bab69c5393d128
luibacli <luibacli@gmail.com>
2023-02-17 18:04:05 +0800

    const PORT to let

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const PORT = process.env.PORT || 5000;
+let PORT = process.env.PORT || 5000;
 const app = express();
 
 connectDB();
