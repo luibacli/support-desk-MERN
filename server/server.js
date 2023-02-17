21def218d6e8db84eecfbeddf2c404ff7dd0ef33
luibacli <luibacli@gmail.com>
2023-02-17 18:12:09 +0800

    changing port

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-let PORT = process.env.PORT || 5000;
+let PORT = process.env.PORT;
 const app = express();
 
 connectDB();
